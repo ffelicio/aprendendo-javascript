6b9cdf5e62d27805f10aaab0765f9ebc9b3f870b
ffelicio <fernando_felicio82@yahoo.com.br>
2017-01-22 23:19:06 -0200

    Operadores matemáticos de forma simplificada.

---
+++ curso-javascript-universidade-xti/operadores-atribuicao/operadores-atribuicao.js
@@ -0,0 +1,25 @@
+/*
+	+= Adição
+	-= Subtração
+	*= Multiplicação
+	/= Divisão
+	%= Módulo
+
+	Faz a operação matemática de forma simplificada
+*/
+
+var a = 6;
+a += 3;
+alert(a);
+
+a -= 3;
+alert(a);
+
+a *= 3;
+alert(a);
+
+a /= 3;
+alert(a);
+
+a %= 3;
+alert(a);
