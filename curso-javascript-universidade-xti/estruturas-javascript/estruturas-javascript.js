76544ac53d049519ed5cbbdf9b47d8f4789216a9
ffelicio <fernando_felicio82@yahoo.com.br>
2017-01-29 14:47:12 -0200

    Explicação sobre as estruturas de controle.

---
+++ curso-javascript-universidade-xti/estruturas-javascript/estruturas-javascript.js
@@ -0,0 +1,21 @@
+/**
+	DECLARAÇÕES PARA ESTRUTURAS DE
+		. CONTROLE
+		. CONDICIONAIS
+		. LOOPS
+		. EXCEÇÕES
+**/
+// estrutura de controle -> var
+var a, b = 2, c; // 'a' e 'c' -> undefined
+
+// estrutura de controle -> function
+function dividir(x, y) {
+	alert(x / y);
+}
+dividir(6, 2);
+
+// estrutura de controle -> return
+function multiplicar(x, y) {
+	return x * y;
+}
+alert(multiplicar(3, 2));
