b6daf51e5a101ca189379677771d21f1d6e135a4
ffelicio <fernando_felicio82@yahoo.com.br>
2017-01-29 16:19:43 -0200

    explicação da estrutura condicional 'switch'.

--- curso-javascript-universidade-xti/estruturas-condicionais-switch/estruturas-condicionais-switch.js
+++ curso-javascript-universidade-xti/estruturas-condicionais-switch/estruturas-condicionais-switch.js
@@ -5,3 +5,32 @@
 		. LOOPS
 		. EXCEÇÕES
 **/
+var sexo = 'M';
+switch( sexo ) {
+	case 'M':
+		alert('Macho');
+		break;
+	case 'F':
+		alert('Femêa');
+		break;
+	default:
+		alert('Indefinido');
+}
+
+var tecnologia = 'Javascript';
+switch( tecnologia ) {
+	case 'Java':
+	case 'C++':
+	case 'Javascript':
+	case 'C#':
+		alert('Linguagem de Programação.');
+		break;
+	case 'SqlServer':
+	case 'PostgreSql':
+	case 'Oracle':
+	case 'MySql':
+		alert('Banco de Dados.');
+		break;
+	default:
+		alert('Tecnologia não conhecida.');
+}
